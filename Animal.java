0cd3a04ecc4fbb14a6c42eec45e73b60d0660275
José Nuno Carvalho <a48542@alunos.ipb.pt>
2024-07-14 01:37:13 +0100

    classe animal e Main

--- Animal.java
+++ Animal.java
@@ -66,4 +66,6 @@ public double calculaExpectativaVida() {
 
     return expectativa;
 }
+
+
 }
--- Main.java
+++ Main.java
@@ -1,23 +1,39 @@
 public class Main {
+
     public static void main(String[] args) {
         Gato gato1 = new Gato("Gatito", "Rafeiro", "Amarelo", 8, 5);
         Cao cao1 = new Cao("bolinhas", "Rafeiro", "Branco", 5, 9);
         Cao cao2 = new Cao("bolao", "Rafeiro", "Branco", 5, 9);
 
-        System.out.print("o " + gato1.nome + " faz " );
+        Animal[] animais = {gato1, cao1, cao2};
+
+        Animal maiorNome = animais[0];
+        for (Animal animal : animais) {
+            if (animal.nome.length() > maiorNome.nome.length()) {
+                maiorNome = animal;
+            }
+        }
+
+        Animal maiorIdade = animais[0];
+        for (Animal animal : animais) {
+            if (animal.idade > maiorIdade.idade) {
+                maiorIdade = animal;
+            }
+        }
+
+        System.out.println("O animal com o maior nome é " + maiorNome.nome);
+        System.out.println("O animal com a maior idade é " + maiorIdade.idade);
+
+        System.out.print("o " + gato1.nome + " faz ");
         gato1.miar();
         System.out.println(gato1.nome + " - Expectativa de vida: " + gato1.calculaExpectativaVida() + " anos");
 
         System.out.print("o " + cao1.nome + " faz ");
         cao1.ladrar();
-        cao1.calculaExpectativaVida();
-
+        System.out.println(cao1.nome + " - Expectativa de vida: " + cao1.calculaExpectativaVida() + " anos");
 
         System.out.print("o " + cao2.nome + " faz ");
         cao2.ladrar();
-        cao2.calculaExpectativaVida();
+        System.out.println(cao2.nome + " - Expectativa de vida: " + cao2.calculaExpectativaVida() + " anos");
     }
-
 }
-
-
